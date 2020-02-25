8707e86c2913ec90538438410ee6f2a7f4718a0f
daperx <jhhoang992@gmail.com>
2020-02-24 23:14:53 -0700

    added pseudocode

---
+++ javascript/game.js
@@ -0,0 +1,32 @@
+//Hangman game 
+// 1 create useable letters and words
+
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var wordBank = ["technology", "books", "entertainment", "learning", "intellect"];
+
+//How can we add these letters and words to our wordboard?
+//How do we generate a board that randomly selects a word from our wordbank?
+//create letters guessed vs letters correct
+
+var guesses = document.onkeypress;
+
+if guesses 
+
+while our word has not been guessed {
+    the game will set our guess counter to 15;
+    the game will randomly select a word from the wordBank;
+    the player will begin to pick a letter;
+    if {
+        letters guess is correct;
+    } then {
+        the letter will be entered on the board;
+        the guess counter will go down;
+    } else if {
+        the letters guessed is incorrect:
+    } then {
+        the letter entered will go to the bottom;
+        the guess counter will go down;
+    }
+};
+
